5c9ed7154674253b977b5d89dd7828f9d891a733
Maiia Gareeva <maiiagareeva@gmail.com>
2025-12-06 23:06:44 -0600

    descriptions update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,11 @@ export default function About(){
         <h2 className="title">ABOUT ME</h2>
         <div className="about-grid">
           <div>
-            <p><strong>Hello!</strong> I'm a passionate developer and photographer who loves to create beautiful and functional experiences.</p>
-            <p>I combine technical expertise with creative vision to build projects that are not only functional but also aesthetically pleasing.</p>
+            <p><strong>Hello!</strong> I'm Maiia Gareeva, ongoing international student in the 
+            University of Minnesota majoring in <strong>Data Science&Statistics</strong>. I'm a passionate developer and photographer 
+            who loves to create CHURMFUL and EFFICIENT experiences.</p>
+            <p>I combine technical expertise with creative vision to build projects that are not only functional but 
+              also aesthetically pleasing. It realizes my creative side despite making music, art, snowbording and more and more.</p>
             <p>I'm also a founder of my podcast <i>BeeInBoots Broadcasts</i> available on {' '}
             <a 
               href="https://apple.co/4awk7hb" 
@@ -33,9 +36,9 @@ export default function About(){
             <h3>What I Do</h3>
             <ul className="skills">
               <li><strong>Web Development</strong><span>HTML, CSS, JavaScript, React, Vite</span></li>
-              <li><strong>Data Science</strong><span>Python, SQL, Java, Machine Learning, LLM</span></li>
-              <li><strong>Photography</strong><span>Landscape, Portrait, Nature</span></li>
-              <li><strong>UI/UX Design</strong><span>Figma, prototyping, user interface</span></li>
+              <li><strong>Data Science</strong><span>Python, SQL, Java, ML, LLM</span></li>
+              <li><strong>Photography</strong><span>Nikon, Canon, Sony, Adobe Lightroom</span></li>
+              <li><strong>UI/UX Design</strong><span>Figma, Adobe <i>Photoshop, Lightroom, Illustrator</i> </span></li>
             </ul>
             <h3>Get In Touch</h3>
             <p>I'm always open to new opportunities and collaborations. Feel free to reach out if you'd like to work together!</p>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,95 +6,111 @@ export default function Projects() {
   const items = [
     {
       title: "Portfolio",
-      desc: "Creating Website - Portfolio using Figma, React, JS and HTML.",
+      desc: "Creating Website - Personal Portfolio with Figma, React, HTML and CSS.",
       img: "/projects_img/main.JPG",
       skills: ["React", "Figma", "CSS", "HTML"],
-      duration: "November 2024",
+      duration: "October-December 2025",
       link: "https://github.com/maiiagareeva/React_website.git",
       longDesc: `
       The website you're using right now is my independent project. Throught it's creation 
       I tried milions of diffent designs, interfaces and inner components.\n
       Ultimately, I designed and built my personal portfolio website from scratch.
       This project started with creating the layout and visual style in Figma, 
-      then bringing those ideas to life using React, JavaScript, and CSS. 
+      then bringing those ideas to life using React, Html, and CSS. 
       I wanted the website being functional and easy to get valuable information quickly. 
-      The website is meant to showcase my projects, photography, and a bit about 
+      The website is meant to showcase my projects, interests like photography/podcast, and a bit about 
       who I am as a data science student.\n 
-      Along the way, I learned how to structure a React app.
+      Along the way, I learned how to structure a React app. This is my first time deploying something in public. 
+      This work could require more and more additions, modifications, updates or UI/UX revisions, 
+      but I'm proudly say that I'm happy releasing it right now!\n
+      Despite that, I'm going to create a user <-> deployer sort of interraction
+       - like chat/chat bot - that will allow everybody give me valuable content-based 
+      suggestions/advices about the website or even my career.\n
       Overall, this project helped me improve my front-end skills, wrap everything I am, I have and 
-      gave me a space I can continue to grow and update over time.`,
+      gave me a space I can continue to grow and update over time.\n
+      Thank you for your time here!`,
     },
     {
       title: "Map API",
       desc: "Integration of Google Map interface into the Gopher Eye App.",
       img: "/projects_img/map.png",
-      skills: ["Dart", "HTML", "Flutter", "Google Maps", "Figma"],
+      skills: ["Dart", "HTML", "Flutter", "Google Maps", "Python"],
       duration: "March–April 2025",
       link: "https://github.com/AgRobotics-Lab-in-BBE-UMN/gopher-eye-app.git",
       longDesc: `
         Gopher Eye App - is a simple flutter-based application for leaves' diseases detection inside
-        the Agriculture Robotics Lab Computer Science team.
+        the Agriculture Robotics Lab Computer Science team.\n
         I built an interactive map tool for the Gopher Eye App which integrates a Google Map. The app
         shows pins of leaves' detection in the fields and lets users drill into each pin to view
-        image evidence, confidence, and suggested actions. 
+        image evidence, confidence, and suggested actions.\n
 
-        This project was integrated inside the Gopher Eye App interface and also helped me develop my design skills.
-        I utilized Figma to make sure the UI/UX and simplicity of using apps tools.`,
+        This project was integrated inside the Gopher Eye App interface and also helped me develop my design skills. 
+        That was my first time working as a part of big research team, I gained an experience of collaborative meeting and brainstorms. 
+        This application is going to be deployed in the App Store soon. Unfortunately, It's currently workable only for IOS user. 
+        My appologies for the rest of the users, we'll work on that problem soon.\n
+        University of Minnesota Twin Cities. Agriculure Robotics Lab Team CS, Ass. Prof. Ce Yang. Research Assistant Nicholas Padilla.`,
     },
     {
-      title: "LLM",
-      desc: "LLM integration to the application of detecting leaves diseases.",
+      title: "VLM",
+      desc: "VLM integration to the application of detecting grape leaves diseases.",
       img: "/projects_img/llm.png",
-      skills: ["Python", "OpenAI API", "Flask", "MSI server", "LoRA Adapter run"],
-      duration: "September-November 2025",
+      skills: ["Python", "OpenAI4o-mini", "Flask", "MSI server", "LoRA Adapter", "Metrics BLEU, ROUGE, BARTScore", "fine-tuning", "dataset preprocess"],
+      duration: "September 2025 - May 2026",
       link: "https://github.com/maiiagareeva/image-cli-bot.git",
       longDesc: `
       Gopher Eye App is a Flutter-based mobile application designed for detecting and 
       analyzing leaves’ diseases, developed within the Agricultural Engineering Lab at the 
-      University of Minnesota. The project aimed to integrate Large Language Model (LLM) 
+      University of Minnesota.\n
+      This project aimed to integrate Large Language Model (LLM) 
       capabilities into the existing system to enhance user interaction and diagnostic insights.\n
-      As part of this work, we implemented a lightweight, locally hosted LLM (Qwen/Qwen3-1.7B) 
-      integrated into the Gopher Eye app via an in-context learning (ICL) pipeline. This setup enables 
-      natural prompt–image interaction for disease classification, improving interpretability and robustness 
-      of the predictions. The model was further optimized using a LoRA adapter to fine-tune visual–text 
-      embeddings through a CLIP framework, enabling efficient prompt–image transformations on limited hardware.`,
+      As part of this work, we're implementing a lightweight, locally hosted LLM (Qwen3-1.7B model) 
+      integrated into the Gopher Eye app. Such integration goes through seaching best model, 
+      LoRA fine-tuning process and finding/creating appropriate dataset of grape leaves diseases. 
+      This setup enables natural prompt–image interaction for disease classification, improving interpretability and robustness 
+      of the predictions. The model is further optimized using a LoRA adapter to fine-tune visual–text 
+      embeddings through a CLIP framework, enabling efficient prompt–image transformations on limited hardware.\n
+      It is the currect project on the stage of "Metrics BLEU, ROUGE, BARTScore research pursuing better and better descriptions guaranteeing results accuracy.\n 
+      University of Minnesota Twin Cities. Agriculure Robotics Lab Team CS, Ass. Prof. Ce Yang. Research Assistant Nicholas Padilla. Udegraduate student, teammate Chang Shen.`,
     },
     {
       title: "Pictured Rocks National Lakeshore",
       desc: "Co-organized a 3-day backpacking for a group of people.",
       img: "/projects_img/PRNL.jpg",
-      skills: ["Planning", "Leadership", "Navigation", "MySQL Workbench"],
+      skills: ["Planning", "Leadership", "Navigation", "SQLight"],
       duration: "September 2025",
       // link: "#",
       longDesc: `
-      Pictured Rocks National Park - is an organized backpacking-hiking trip in September 2025.
-      I co-organized a 3-day backpacking trip by building a 
-      SQL-driven system. 
-      First and foremost, I designed and managed 
-      relational tables to track participant information, experience levels, 
+      Pictured Rocks National Lakeshore - is located in the Michigan, United States with a beautifull trails near by the Superior Lake.\n
+      I co-organized a 3-day backpacking trip utilizing a SQL-driven system.\n
+      It was my third co-orginized trip so I've already known basic steps which helped me in organizing information a lot.
+      First and foremost, I designed and managed relational tables to track participant information, experience levels, 
       emergency contacts, and equipment inventory. Despite that, I used SQL queries to 
-      identify gear shortages, balance pack weight, and ensure all safety 
+      identify gear shortages and its price, balance pack weight, and ensure all safety 
       requirements were met. Structured trail-segment data (distance, 
-      possible elevation, water sources) and queried it to optimize daily routes and 
+      possible elevation, water sources and campground with their prices as well) and queried it to optimize daily routes and 
       campsite planning. Created cost-breakdown queries to manage the 
-      trip budget and calculate per-person expenses. 
-      
+      trip budget and calculate per-person expenses.\n
       To sum up, I created thorough instructions that improved 
-      coordination across co-organizers and participants calmness.`,
+      coordination across co-organizers and participants calmness. That was one of the best backpacking trips I had combining
+      a great people, new experience and awesome views. We also had a couple of difficulties during that travel, 
+      but I suggest they were minimized by our organization and dedication to the process.\n
+      Some photos from that trip are (or will be) available in the Photography Section on that website. See photos available captions.`,
     },
     {
       title: "High School Photoset",
       desc: "Made a personal requested photoset for a girl.",
       img: "/projects_img/photoset.jpg",
-      skills: ["Photography", "Portraits", "Canon Rebel DSLR"],
+      skills: ["Photography", "Portraits", "Canon Rebel DSLR", "Adobe Lightroom"],
       duration: "May 2025",
       longDesc: `
-      I created a high school senior photoset in Saint Paul Park, 
+      I'm starting a path of passionate phorographer and always open to new practices.
+      I've already created a high school senior photoset in Saint Paul Park, Minnesota, United States, 
       capturing a variety of portraits across different locations, 
       angles, and setups. The goal was to produce a personalized 
       graduation album with both styled and natural shots, 
-      using props and simple compositions to highlight her personality. I used 
-      Canon EF-S 18–55mm f/3.5–5.6.
+      using props and simple compositions to highlight her personality. Such experience of working 
+      with the model was new and interesting, showing up earlier hidden challenges and opportunities to grow.\n
+      I used Canon EF-S 18–55mm f/3.5–5.6.
       `,
     },
   ];
